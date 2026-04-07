ee14963da9a45cee3ed79d3d4825a100dff033f1
abdou0dz <abdouabdou0dz0dz@gmail.com>
2026-04-07 12:29:55 +0200

    here I regisered the app by going to providers/appserviceprovider.php and register the new service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->singleton('welcome', function ($app) {
+            return new \App\Services\WelcomeService();
+        });
     }
 
     /**
